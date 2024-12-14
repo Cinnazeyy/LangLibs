d0b34d3ed4662cfe6bc7e8d0a39eafc54ed70089
Cinnazeyy <imcinnazeyy@gmail.com>
2024-12-14 22:37:07 +0100

    remove all ui stuff, as thats covered by the LangUI

--- src/main/java/li/cinnazeyy/langlibs/LangLibs.java
+++ src/main/java/li/cinnazeyy/langlibs/LangLibs.java
@@ -1,12 +1,10 @@
 package li.cinnazeyy.langlibs;
 
-import li.cinnazeyy.langlibs.commands.CMD_Language;
 import li.cinnazeyy.langlibs.core.DatabaseConnection;
 import li.cinnazeyy.langlibs.core.EventListener;
 import li.cinnazeyy.langlibs.util.LangUtils;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandMap;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -43,10 +41,6 @@ public final class LangLibs extends JavaPlugin {
 
         // Register Events
         getServer().getPluginManager().registerEvents(new EventListener(), this);
-
-        // Register Commands
-        final CommandMap commandMap = Bukkit.getServer().getCommandMap();
-        if (config.getBoolean("languageSelection.enabled")) commandMap.register("language", new CMD_Language("language"));
 
         // Plugin startup logic
         Bukkit.getConsoleSender().sendMessage(text("[", NamedTextColor.DARK_GRAY)
